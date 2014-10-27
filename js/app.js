38a778c220426a27ddefc074a69c9143b64185a7
avinnakota <avinnakotaster@gmail.com>
2014-10-26 20:47:11 -0700

    Latest version-works

--- js/app.js
+++ js/app.js
@@ -1,30 +1,18 @@
-$(document).ready(function(){
-function input = function(limit)
-{
-    limit = prompt("Please enter a number: ","0");
-
-    for (var i = 1; i <= parseInt(limit); i++)
-    {
-        if (i % 5 == 0 && i % 3 == 0)
-        {
-            $("#game").append("<p>fizzbuzz</p>");
-         }
-    
-         else if (i % 3 == 0)
-        { 
-            $("#game").append("<p>fizz</p>");
-        }   
-    
-        else if (i % 5 == 0)
-        {
-             $("#game").append("<p>fizz</p>");
-        }
-    
-        else
-        {
-            $("#game").append("<p>" + i + "</p>");
-        }   
-    }
-}
- input();
+$(document).ready(function() {
+    var l = prompt("Please enter a number: ", "0");
+    input(l);
 });
+    
+var input = function(l) {
+        for (var i = 1; i <= parseInt(l); i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                $("#game").append("<p>fizzbuzz</p>");
+            } else if (i % 3 == 0) {
+                $("#game").append("<p>fizz</p>");
+            } else if (i % 5 == 0) {
+                $("#game").append("<p>fizz</p>");
+            } else {
+                $("#game").append("<p>" + i + "</p>");
+            }
+        }
+    };
